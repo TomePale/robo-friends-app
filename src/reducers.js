26a7dc39364132dece5fb871508cf60ca5784f99
TomePale <tome.perica@yahoo.com>
2019-02-28 00:48:15 +0100

    Working on Redux

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,7 @@
 import { CHANGE_SEARCH_FIELD } from './constants';
 
 const initialState = {
-    searchFiled: ''
+    searchField: ''
 }
 
 
@@ -9,9 +9,7 @@ export const searchRobots = (state=initialState, action={}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, {searchField: action.payload})
-            break;
         default:
             return state;
-            break;
     }
 }
